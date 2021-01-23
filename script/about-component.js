a5d86013ba13ea10403e351c60bda18e28469404
Ibne Nahian <evilprince2009@gmail.com>
2021-01-24 01:57:01 +0600

    change

--- script/about-component.js
+++ script/about-component.js
@@ -6,7 +6,7 @@ export class AboutComponent {
         const aboutTemplate = document.createElement("div");
         aboutTemplate.classList.add("intro");
         aboutTemplate.innerHTML = `
-        <p>Hello,<br>I'm <span style="color: aquamarine;">Ibne Nahian</span>, a code geek , music lover , traveller & obviously a passionate learner. Worst thing about me is <span style="color: #6ef500; font-style:oblique;">'A little stupid Bug can give me Insomnia'</span>. Well, I think that's enough introduction.
+        <p>Hello,<br>I'm <span style="color: aquamarine;">Ibne Nahian</span>, a code freak , music lover , traveller & obviously a passionate learner. Worst thing about me is <span style="color: #6ef500; font-style:oblique;">'A little stupid Bug can give me Insomnia'</span>. Well, I think that's enough introduction.
         </p>
         `;
         this.container.appendChild(aboutTemplate);
